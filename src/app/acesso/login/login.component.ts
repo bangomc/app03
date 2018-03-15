cda9fd69f91ce0d64bc7c23f1f58c7119ad46909
Cristhiano Roberto Carvalho Pinto <crpinto1@stefanini.com>
2018-03-15 13:10:35 -0300

    Adicionado login com Google

--- src/app/acesso/login/login.component.ts
+++ src/app/acesso/login/login.component.ts
@@ -34,4 +34,8 @@ export class LoginComponent implements OnInit {
     );
   }
 
+  signinGoogle(){
+    this.autenticacao.signinGoogle();
+  }
+
 }
--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -15,26 +15,34 @@ export class Autenticacao {
     cadastrarUsuario(usuario: Usuario): Promise<any>{
         return firebase.auth().createUserWithEmailAndPassword(usuario.email,usuario.senha)
             .then((resposta: any) => {
-                delete usuario.senha;
-                firebase.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)
-                    .set(usuario)
+               this.persistirDetalheUsuario(usuario);
             })
             .catch((erro: Error)=>{
                 console.log(erro);
             })
     }
 
+    persistirDetalheUsuario(usuario: Usuario){
+        delete usuario.senha;
+        firebase.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)
+            .set(usuario)
+    }
+
     autenticar(email: string, senha: string) {
         firebase.auth().signInWithEmailAndPassword(email,senha)
             .then((resposta: any) => { 
-                firebase.auth().currentUser.getIdToken()
-                    .then((idToken: string) => {
-                        this.token_id = idToken;
-                        localStorage.setItem('idToken', idToken);
-                        this.router.navigate(['/home']);
-                    })
+                this.gravarToken()
             })
             .catch((erro: Error) => { console.log(erro)})
+    }
+
+    gravarToken(){
+        firebase.auth().currentUser.getIdToken()
+            .then((idToken: string) => {
+                this.token_id = idToken;
+                localStorage.setItem('idToken', idToken);
+                this.router.navigate(['/home']);
+            })
     }
 
     autenticado(): boolean {
@@ -52,6 +60,31 @@ export class Autenticacao {
                 this.router.navigate(['/']);
             });
         
+    }
+
+    signinGoogle() {
+        if(!firebase.auth().currentUser){
+            let provider: firebase.auth.GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
+            provider.addScope('profile');
+            provider.addScope('email');
+            firebase.auth().signInWithPopup(provider)
+                .then((result) => {
+                    const googleUserProfile = result.additionalUserInfo.profile;
+                    const usuario: Usuario = new Usuario(
+                        googleUserProfile.email,
+                        googleUserProfile.name,
+                        googleUserProfile.given_name,''
+                    );
+                    this.persistirDetalheUsuario(usuario);                    
+                    console.log(result);
+                    this.gravarToken();
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        }else{
+            this.sair();
+        }
     }
 
 }
